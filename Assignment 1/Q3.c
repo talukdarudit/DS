3c5043134ae5d2c78809cc6b82db2e5006839c3f
talukdarudit <78261320+talukdarudit@users.noreply.github.com>
2021-10-10 23:38:32 +0530

    Update Q3.c

--- Assignment 1/Q3.c
+++ Assignment 1/Q3.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+void convertTo2D(int *arr,int **res int rows, int columns){
+    int k=0;
+    int res[rows][columns];
+    for(int i=0; i<rows; ++i){
+        for(int j=0; j<columns; ++j){
+            res[i][j]=arr[k];
+            k++;
+        }
+    }
+}
+
+void print2DArray(int **res, int rows, int columns){
+    for(int i=0; i<rows; ++i){
+         for(int j=0; j<columns; ++j){
+            printf("%d\t", &res[i][j]);
+         }
+         printf("\n");
+    }
+
+int main(){
+    int rows, columns;
+    scanf("%d %d", &rows, &columns);
+    int arr[rows*cols];
+    int res[rows][columns];
+    for(int i=0; i<rows*cols; ++i){
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
+    convertTo2D(arr, rows, columns);
+    print2DArrayy(res, rows, columns);
+    return 0;
+}
