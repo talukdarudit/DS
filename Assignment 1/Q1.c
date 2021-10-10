5b2fb6dba7e144567f438e043bc5a281524223c5
talukdarudit <78261320+talukdarudit@users.noreply.github.com>
2021-10-10 23:58:48 +0530

    Update Q1.c

--- Assignment 1/Q1.c
+++ Assignment 1/Q1.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int strlen(char* str){
+    int len=0;
+    while(str[len]!='\0'){
+        len++;
+    }
+    return len;
+}
+
+int isValidFloat(char* num){
+    for(int i=0; i<strlen(num); ++i){
+        if(num[i]=='.'){
+            if(num[i+1]=='\0'){
+                return 0;
+            }
+            else{
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main(){
+    char num[20];
+    scanf("%c", &num);
+
+    if(isValidFloat(num)==1){
+        printf("Valid\n");
+    }
+    else{
+        printf("Invalid\n");
+    }
+    return 0;
+}
