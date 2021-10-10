86575995d10879da37ff58daf07454890ce9f2b1
talukdarudit <78261320+talukdarudit@users.noreply.github.com>
2021-10-10 23:49:55 +0530

    Update Q3.c

--- Assignment 1/Q3.c
+++ Assignment 1/Q3.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 
-void convertTo2D(int *arr,int **res, int rows, int columns){
-    int k=0;
+void convertTo2D(int arr[], int rows, int cols){
     for(int i=0; i<rows; ++i){
-        for(int j=0; j<columns; ++j){
-            res[i][j]=arr[k];
-            k++;
+        for(int j=0; j<cols; ++j){
+            printf("%d ", arr[i*cols+j]);
         }
+        printf("\n");
     }
 }
 
-void print2DArray(int **res, int rows, int columns){
-    for(int i=0; i<rows; ++i){
-         for(int j=0; j<columns; ++j){
-            printf("%d\t", &res[i][j]);
-         }
-         printf("\n");
-    }
-
 int main(){
-    int rows, columns;
-    scanf("%d %d", &rows, &columns);
-    int arr[rows*columns];
-    int res[rows][columns];
-    for(int i=0; i<rows*columns; ++i){
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+    int arr[rows*cols];
+    for(int i=0; i<rows*cols; ++i){
         scanf("%d", &arr[i]);
     }
     printf("\n");
-    convertTo2D(arr, rows, columns);
-    print2DArray(res, rows, columns);
+    convertTo2D(arr, rows, cols);
     return 0;
 }
